1c4cbecdb38fe5cbbe378bed82fe32908c562121
Reza Soleymanifar <reza@soleymanifar.com>
2023-03-20 17:59:15 -0500

    baseline

---
+++ meta/data.py
@@ -0,0 +1,10 @@
+from typing import List
+
+class DataHandler():
+    def __init__(self) -> None:
+        pass
+    def download(symbols: List[str], interval: str, start: str, end: str)
+
+class FeatureEngineer():
+    def __init__(self) -> None:
+        pass
