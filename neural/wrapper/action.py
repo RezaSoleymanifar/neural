e287f5222c84ad1f586d3a1c4a731ae53ebdc6a0
Reza Soleymanifar <reza@soleymanifar.com>
2023-05-21 14:27:59 -0500

    --- Auto Git Commit ---

--- neural/wrapper/action.py
+++ neural/wrapper/action.py
@@ -494,18 +494,23 @@ class InitialMarginActionWrapper(ActionWrapper):
     def action(self, actions: np.ndarray[float]) -> np.ndarray[float]:
         """
         Checks if initial margin requirement is met for all assets. If
-        not then randomly sets a nonzero action to zero. This is a 
-        conservative approach to ensure that initial margin requirement
-        firstly satisfied, because if excess margin is greater than
-        initial margin requirement then it by definition marginable 
-        equity is greater than initial margin requirement. Secondly
-        post transaction excess margin is greater than zero, which
-        ensures that no margin call is received.
+        not then sets all porfolio increasing actions to zero. In the
+        context of marginable assets
+        This is a conservative approach to ensure that initial margin
+        requirement firstly satisfied, because if excess margin is
+        greater than initial margin requirement then it by definition
+        marginable equity is greater than initial margin requirement.
+        Secondly post transaction excess margin is greater than zero,
+        which ensures that no margin call is received.
         ExcessMarginActionWrapper can be used to ensure that this rule
         is not triggered, by modifying the agent's actions that lead to
         increasing portfolio value. In case this mediation is not enough
-        then following rule is triggered to ensure that a base level
-        of liquidity is maintained at all times.
+        then following rule is triggered to ensure that a base level of
+        liquidity is maintained at all times.
+
+        In the context of nonmarginable assets this rule ensures that
+        there is enough cash to increase the position value of
+        nonmarginable assets.
         """
         excess_margin = self.market_metadata_wrapper.excess_margin
         if excess_margin < self.initial_margin_required:
