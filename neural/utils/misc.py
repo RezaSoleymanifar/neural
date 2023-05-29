acc363eecfbb77ca548597a0b7607bb2d9123ab7
Reza Soleymanifar <reza@soleymanifar.com>
2023-05-29 12:06:45 -0500

    --- Auto Git Commit ---

--- neural/utils/misc.py
+++ neural/utils/misc.py
@@ -1,5 +1,23 @@
 """
-misc.py
+misc.py 
+
+Miscellaneous utility functions. These are less generic more use-case
+specific facilities.
+
+Methods:
+--------
+    objects_list_to_dataframe(objects_list: List[object]) ->
+    pd.DataFrame:
+        Converts a collection of objects into a pandas DataFrame. Uses
+        dictionary representation of the objects to create the
+        dataframe. Used in the AlpacaClient class to convert the assets
+        objects into a DataFrame, or convert positions objects into a
+        dataframe. If a value is Enum, its string representation is used
+        in dataframe.
+    
+    to_timeframe(timeframe: str) -> TimeFrame:
+        Parses a string representation of a timeframe into a TimeFrame
+        object.
 """
 from typing import List
 from enum import Enum
@@ -60,12 +78,11 @@ def to_timeframe(timeframe: str):
             A TimeFrame object representing the parsed time frame.
 
     Raises:
+    --------
         ValueError: If the input string is not a valid time frame. Valid
         time frame examples include
             '59Min', '23Hour', '1Day', '1Week', and '12Month'.
     """
-    
-
     match = re.search(r'(\d+)(\w+)', timeframe)
 
     if not match:
