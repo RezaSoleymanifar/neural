4fb7bb03e33a944f58995315bd88b65ed6a429c1
Reza Soleymanifar <reza@soleymanifar.com>
2023-08-05 16:32:49 -0500

    --- Auto Git Commit ---

--- neural/model/base.py
+++ neural/model/base.py
@@ -1,2 +1,40 @@
+from gym import spaces
 
 
+class AbstractModel:
+    """
+    This is the base class for all models.
+    """
+
+    def __init__(self):
+        """
+        Initialize the model.
+        """
+        pass
+
+    def __call__(self, observation):
+        """
+        Given an observation, return an array of actions.
+
+        args:
+        ----------
+        observation (numpy.ndarray): 
+            The observation from the environment.
+
+        Returns:
+        ----------
+        numpy.ndarray: 
+            An array of actions.
+        """
+        raise NotImplementedError
+
+    def save(self, file_path):
+        """
+        Save the model to a file.
+
+        Parameters:
+        ----------
+        file_path (str): 
+            Path to save the model.
+        """
+        raise NotImplementedError
