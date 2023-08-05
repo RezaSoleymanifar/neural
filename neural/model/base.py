affd79e0481e486849184e7a5d47acab0f357ee0
Reza Soleymanifar <reza@soleymanifar.com>
2023-08-05 17:03:20 -0500

    --- Auto Git Commit ---

--- neural/model/base.py
+++ neural/model/base.py
@@ -39,3 +39,43 @@ class AbstractModel:
             Path to save the model.
         """
         raise NotImplementedError
+    
+    def train(self, )
+
+class ActorCriticModel(AbstractModel):
+    """
+    This is the base class for all models.
+    """
+
+    def __init__(self):
+        """
+        Initialize the model.
+        """
+        pass
+
+    def __call__(self, observation):
+        """
+        Given an observation, return an array of actions.
+
+        args:
+        ----------
+        observation (numpy.ndarray): 
+            The observation from the environment.
+
+        Returns:
+        ----------
+        numpy.ndarray: 
+            An array of actions.
+        """
+        raise NotImplementedError
+
+    def save(self, file_path):
+        """
+        Save the model to a file.
+
+        Parameters:
+        ----------
+        file_path (str): 
+            Path to save the model.
+        """
+        raise NotImplementedError
