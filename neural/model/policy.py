4e7fe96e42e44191a135f0e4b098c6b56245d425
Reza Soleymanifar <reza@soleymanifar.com>
2023-08-06 15:43:24 -0500

    --- Auto Git Commit ---

---
+++ neural/model/policy.py
@@ -0,0 +1,27 @@
+
+from stable_baselines3.common.policies import ActorCriticPolicy
+
+
+class CustomActorCriticPolicy(ActorCriticPolicy):
+    def __init__(
+        self,
+        observation_space: spaces.Space,
+        action_space: spaces.Space,
+        lr_schedule: Callable[[float], float],
+        *args,
+        **kwargs,
+    ):
+        # Disable orthogonal initialization
+        kwargs["ortho_init"] = False
+        super().__init__(
+            observation_space,
+            action_space,
+            lr_schedule,
+            # Pass remaining arguments to base class
+            *args,
+            **kwargs,
+        )
+
+
+    def _build_mlp_extractor(self) -> None:
+        self.mlp_extractor = CustomNetwork(self.features_dim)
